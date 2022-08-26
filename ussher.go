e500302d4c17ae7fd54b31666a7a53c1e7f00934
Dolph Mathews <dolph.mathews@gmail.com>
2022-08-26 18:40:59 -0500

    flatten package

--- ussher.go
+++ ussher.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/dolph/ussher/pkg/cmd"
-	"github.com/dolph/ussher/pkg/config"
+	"github.com/dolph/ussher/cmd"
+	"github.com/dolph/ussher/config"
 )
 
 func main() {
